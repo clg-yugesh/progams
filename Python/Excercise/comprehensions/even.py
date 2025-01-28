ebdd723a68c818482a9c827b97ac8974ead70923
Yugesh <242237@slcs.edu.in>
2025-01-28 11:40:38 +0530

    html

---
+++ Python/Excercise/comprehensions/even.py
@@ -0,0 +1,7 @@
+#to print only the even numbers
+
+input_list = [1, 2, 3, 4, 4, 5, 6, 6, 6, 7, 7]
+
+opu = [var for var in input_list if var % 2 == 0 ]
+
+print(opu)
---
+++ Python/Excercise/comprehensions/list_comp.py
@@ -0,0 +1,5 @@
+inp = [1,2,3,4,4,5,6,7,7]
+
+oup = [var for var in inp if var % 2 == 0]
+
+print(oup)
